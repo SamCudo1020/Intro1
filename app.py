6c5a28687bc9b991f52a324532de0dafdc55f1a2
SamCudo1020 <chamuel567@gmail.com>
2026-02-26 10:06:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,6 @@ st.title("Esta es mi primera app en la nube")
 
 st.header("En este espacio comienzo a desarrollar mis aplicaciones.")
 st.write("Facilmente puedo realizar backend y frontend.")
-image = Image.open('
+image = Image.open('carita_feliz.jpg')
+st.image(image, caption='Interfaces multimodales')
+                   
