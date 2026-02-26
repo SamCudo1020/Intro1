0bae1d5e8196001a69103029cc676333a9bda1a9
SamCudo1020 <chamuel567@gmail.com>
2026-02-26 10:03:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.title("Esta es mi primera app en la nube")
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones.")
+st.write("Facilmente puedo realizar backend y frontend.")
+image = Image.open('
